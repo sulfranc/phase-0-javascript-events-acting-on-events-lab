a3ecb07f6da696686bf31c3ee1c20fc97032ca26
Jeff Sulfranc <sulfranc@gmail.com>
2021-12-15 13:17:30 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.querySelector('#dodger')
+
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+    moveDodgerLeft()
+  } 
+     
+    else if (event.key === "ArrowRight") {
+          moveDodgerRight()
+        }
+})
+
+function moveDodgerLeft() {
+          const leftNumbers = dodger.style.left.replace("px", "");
+          const left = parseInt(leftNumbers, 10);
+
+          if (left > 0) {
+            dodger.style.left = `${left - 10}px`;
+          }
+          
+}
+
+function moveDodgerRight() {
+          const leftNumbers = dodger.style.left.replace("px", "");
+          const left = parseInt(leftNumbers, 10);
+
+          if (left > 0) {
+            dodger.style.left = `${left + 10}px`;
+          }
+
+
+}
